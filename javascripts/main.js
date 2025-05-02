ff563a3a3c36f0842906d7c38d20743a99be0d6c
Abi Lutfiandy <abilutfiandyy@gmail.com>
2025-05-02 18:54:43 +0700

    update Portfolio

--- javascripts/main.js
+++ javascripts/main.js
@@ -32,4 +32,39 @@ function copyIg() {
     );
 }
 
+// hamburger menu
+
+const menu = document.querySelector('.menu');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const iconBars = document.querySelector('.icon-bars');
+const iconClose = document.querySelector('.icon-close');
+
+hamburgerMenu.addEventListener('click', displayMenu);
+menu.addEventListener('click', displayMenu);
+
+function displayMenu() {
+    if (menu.classList.contains('tampil')) {
+        menu.classList.remove('tampil');
+        iconBars.style.display = 'inline';
+        iconClose.style.display = 'none';
+    } else {
+        menu.classList.add('tampil');
+        iconBars.style.display = 'none';
+        iconClose.style.display = 'inline';
+    }
+}
+
 // typing efect
+
+document.addEventListener('DOMContentLoaded', function () {
+    new Typed('#typing-text', {
+        strings: ['Web Developer', 'Frontend Enthusiast', 'Laravel Programmer'],
+        typeSpeed: 50,
+        backSpeed: 30,
+        backDelay: 2000,
+        loop: true,
+        smartBackspace: true,
+        showCursor: true,
+        cursorChar: '|',
+    });
+});
